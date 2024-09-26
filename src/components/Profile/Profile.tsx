da9fd2078a25595d77d14b7b61753f036e3e9981
JackPadalino <pada0867@gmail.com>
2024-09-26 10:38:23 -0400

    removed unused imports and variables

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,31 +3,36 @@ import { onAuthStateChanged } from "firebase/auth";
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 import { auth, db } from "../../../firebase";
-import LocationInput from "./LocationInput";
+// import LocationInput from "./LocationInput";
 
-import { Flex, Text, Input, Textarea } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  // Input,
+  // Textarea
+} from "@chakra-ui/react";
 import "./profile.css";
 
 // user profile component - a user can view/edit their personal information
 // here
 const Profile = () => {
   const [user, setUser] = useState<any>(null);
-  const [profileCompletion, setProfileCompletion] = useState<number>(0);
+  // const [profileCompletion, setProfileCompletion] = useState<number>(0);
 
-  // calculating how much of a user's profile is complete based on number of
-  // completed form inputs
-  const calculateProfileCompletion = (userInfo: any) => {
-    const userAttributes = ["name", "email", "classes", "location", "about"];
-    const attributesCompleted = userAttributes.filter((attr) => {
-      return Array.isArray(userInfo[attr])
-        ? userInfo[attr].length > 0
-        : !!userInfo[attr];
-    });
+  // // calculating how much of a user's profile is complete based on number of
+  // // completed form inputs
+  // const calculateProfileCompletion = (userInfo: any) => {
+  //   const userAttributes = ["name", "email", "classes", "location", "about"];
+  //   const attributesCompleted = userAttributes.filter((attr) => {
+  //     return Array.isArray(userInfo[attr])
+  //       ? userInfo[attr].length > 0
+  //       : !!userInfo[attr];
+  //   });
 
-    const percentComplete =
-      (attributesCompleted.length / userAttributes.length) * 100;
-    setProfileCompletion(percentComplete);
-  };
+  //   const percentComplete =
+  //     (attributesCompleted.length / userAttributes.length) * 100;
+  //   setProfileCompletion(percentComplete);
+  // };
 
   // checking for user locally -> need to add user object to recoil or redux store
   // when logging in and then pull down to this component later
@@ -39,7 +44,7 @@ const Profile = () => {
         await getDocs(q).then((querySnapshot) => {
           const userDoc = querySnapshot.docs[0];
           const userInfo = userDoc.data();
-          calculateProfileCompletion(userInfo);
+          // calculateProfileCompletion(userInfo);
           setUser(userInfo);
         });
       }
